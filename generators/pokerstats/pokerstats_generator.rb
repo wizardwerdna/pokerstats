2490de6cf2e4a71e1af0135be15c0feb07a65d86
Andrew C. Greenberg <wizardwerdna@gmail.com>
2009-08-29 03:12:28 -0700

    rename migration files bug

--- generators/pokerstats/pokerstats_generator.rb
+++ generators/pokerstats/pokerstats_generator.rb
@@ -2,8 +2,8 @@ class PokerstatsGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.directory "db/migration"
-      m.template "hand_statistics.rhtml",   "db/migration/create_hand_statistics"
-      m.template "player_statistics.rhtml",   "db/migration/create_player_statistics"
+      m.template "hand_statistics.rhtml",   "db/migration/create_hand_statistics.migration"
+      m.template "player_statistics.rhtml",   "db/migration/create_player_statistics.migration"
     end
   end
 end
